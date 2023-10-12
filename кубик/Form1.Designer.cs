b4d41a0dd7d3c78c880463750e58ba8d4ee40dd0
Илья Тупицин <ilhatupitsin@yandex.ru>
2023-10-12 11:40:00 +0300

    Добавьте файлы проекта.

---
+++ кубик/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace кубик
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(695, 634);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(808, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(356, 183);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "кинуть кубик первому игроку";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(808, 361);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(356, 183);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "кинуть кубик второму игроку";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(1176, 438);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Вам выпало";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(1368, 438);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 29);
+            this.label2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(1176, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Вам выпало ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(1368, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 29);
+            this.label4.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(942, 266);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 29);
+            this.label5.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(808, 652);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(356, 50);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "сбросить счёт";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1534, 782);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ кубик/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace кубик
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        public void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int player1Roll = random.Next(1, 7);
+            switch (player1Roll)
+            {
+                case 6:
+                    pictureBox1.Image = Image.FromFile("C:\\Users\\Пользователь\\Desktop\\dice-roll-dice.gif");
+                    label4.Text = "6";
+                    break;
+                case 5:
+                    pictureBox1.Image = Image.FromFile("C:\\Users\\Пользователь\\Desktop\\dice-roll-dice (1).gif");
+                    label4.Text = "5";
+                    break;
+                case 4:
+                    pictureBox1.Image = Image.FromFile("C:\\Users\\Пользователь\\Desktop\\dice-roll-dice (4).gif");
+                    label4.Text = "4";
+                    break;
+                case 3:
+                    pictureBox1.Image = Image.FromFile("C:\\Users\\Пользователь\\Desktop\\dice-roll-dice (2).gif");
+                    label4.Text = "3";
+                    break;
+                case 2:
+                    pictureBox1.Image = Image.FromFile("C:\\Users\\Пользователь\\Desktop\\dice2-dice.gif");
+                    label4.Text = "2";
+                    break;
+                case 1:
+                    pictureBox1.Image = Image.FromFile("C:\\Users\\Пользователь\\Desktop\\dice-roll-dice (3).gif");
+                    label4.Text = "1";
+                    break;
+            }
+            
+                if (label4.Text.Length != 0) button1.Enabled = false;
+                else button1.Enabled = true;
+            
+        }
+
+        public void button2_Click(object sender, EventArgs e)
+        {
+           
+         
+            Random random = new Random();
+            int player2Roll = random.Next(1, 7);
+            switch (player2Roll)
+            {
+                case 6:
+                    pictureBox1.Image = Image.FromFile("C:\\Users\\Пользователь\\Desktop\\dice-roll-dice.gif");
+                    label2.Text = "6";
+                    break;
+                case 5:
+                    pictureBox1.Image = Image.FromFile("C:\\Users\\Пользователь\\Desktop\\dice-roll-dice (1).gif");
+                    label2.Text = "5";
+                    break;
+                case 4:
+                    pictureBox1.Image = Image.FromFile("C:\\Users\\Пользователь\\Desktop\\dice-roll-dice (4).gif");
+                    label2.Text = "4";
+                    break;
+                case 3:
+                    pictureBox1.Image = Image.FromFile("C:\\Users\\Пользователь\\Desktop\\dice-roll-dice (2).gif");
+                    label2.Text = "3";
+                    break;
+                case 2:
+                    pictureBox1.Image = Image.FromFile("C:\\Users\\Пользователь\\Desktop\\dice2-dice.gif");
+                    label2.Text = "2";
+                    break;
+                case 1:
+                    pictureBox1.Image = Image.FromFile("C:\\Users\\Пользователь\\Desktop\\dice-roll-dice (3).gif");
+                    label2.Text = "1";
+                    break;
+
+            }
+            
+            int a = int .Parse(label2.Text);
+            int b = int .Parse(label4.Text);
+            if(a > b)
+            {
+                label5.Text = "победил игрок номер 2";
+            }
+            if (a < b)
+            {
+                label5.Text = "победил игрок номер 1";
+            }
+            if (a == b)
+            {
+                label5.Text = "ничья";
+            }
+            {
+                if (label2.Text.Length != 0 ) button2.Enabled = false;
+                else button1.Enabled = true;
+            }
+           
+
+
+        }
+        
+        public void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label4.Text = "";
+            label2.Text = "";
+            label5.Text = "";
+            pictureBox1.Image = null;
+            button1.Enabled = true;
+            button2.Enabled = true;
+        }
+    }
+}
+
